031ec69e171ca8f3ed45fa5dc3b0eb48c78d9eac
M1 <22069@supnum.mr>
2024-03-16 22:09:32 +0000

    update creation user

--- S3C/tach1/urls.py
+++ S3C/tach1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('login/',views.login,name='login'),
+   path('creation_user/',views.creation_user,name='creation_user'),
 ]
