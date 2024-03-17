2c34a108cc59c3991cc851925c57a17a71d5fb2d
M1 <22069@supnum.mr>
2024-03-17 13:07:45 +0000

    n'est pas verifier le compte

--- S3C/tach1/urls.py
+++ S3C/tach1/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
   
    # _________________________________JURY______________________________________________________________
    path('creation_jury/',views.creation_jury,name='creation_jury'),
-   path('verification_jury/',views.verification_jury,name='verification_jury'),
    path('liste_jury/',views.liste_jury,name='liste_jury'),
    path('supprimer_jury/<int:id_jury>',views.supprimer_jury,name="supprimer_jury"),
    path('modifier_jury/<int:id_jury>',views.modifier_jury,name="modifier_jury"),
@@ -22,7 +21,6 @@ urlpatterns = [
    
       # _________________________________ADMIN______________________________________________________________
    path('creation_admin/',views.creation_admin,name='creation_admin'),
-   path('verification_admin/',views.verification_admin,name='verification_admin'),
    path('liste_admin/',views.liste_admin,name='liste_admin'),
    path('supprimer_admin/<int:id_admin>',views.supprimer_admin,name="supprimer_admin"),
    path('modifier_admin/<int:id_admin>',views.modifier_admin,name="modifier_admin"),
