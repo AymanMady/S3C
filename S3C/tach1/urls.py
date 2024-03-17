6c289125f52aa360a73ec0d5413dde74c0570139
aliywedad <aliysidahmedwedad@gmail.com>
2024-03-17 09:21:01 -1200

    j

--- S3C/tach1/urls.py
+++ S3C/tach1/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
    path('creation_user/',views.creation_user,name='creation_user'),
+   path('import_etudiantsl/',views.import_etudiantsl,name='import_etudiantsl'),
 ]
--- S3C/tach1/views.py
+++ S3C/tach1/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,HttpResponse
 from main.models import *
-# Create your views here.
-
+import openpyxl ,os
 def creation_user(request):
     if request.method == 'POST':
         nom = request.POST.get('nom')
@@ -22,3 +21,21 @@ def creation_user(request):
         )
         return HttpResponse("insere")
     return render(request, "creation_user.html")
+
+
+def import_etudiantsl(request):
+    if request.method == "POST" and request.FILES.get('excel_file'):
+        excel_file = request.FILES['excel_file']
+        wb = openpyxl.load_workbook(excel_file)
+        emails = Etudiant.objects.values_list('email', flat=True)
+
+        sheet = wb.active
+
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            if row[2] not in emails  :
+                obj = Etudiant(nom=row[0], prénom=row[1],  email=row[2], spécialité=row[3], niveau=row[4])  # Adjust field names accordingly
+                obj.save()
+
+        print('Data imported successfully')
+        return render(request, "import_etudiant.html")
+    return render(request, "import_etudiant.html")
